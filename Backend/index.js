5b423f9bda110706ed77ffde1b0aeb03d9c0ebae
Shanto57575 <shanto57575@gmail.com>
2023-12-03 16:41:52 +0600

    update api added

--- Backend/index.js
+++ Backend/index.js
@@ -29,6 +29,8 @@ async function run() {
     try {
         await client.connect();
 
+        //creating books collection in DATABASE
+
         const usersDataCollection = client.db("USER_DB").collection("users_data");
 
         // add a user in the database
